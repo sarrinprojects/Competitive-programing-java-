e08b98fb312f449afe45b01b30a5ad591d855d52
sarrin lal <134491122+sarrinprojects@users.noreply.github.com>
2026-02-10 06:26:23 +0000

    updated

---
+++ lab7.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+public class lab7{
+    public static void main(String[] args){
+        int[] arr={12,15,10,18,14};
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]+"");
+        }
+
+
+    }
+
+
+}
